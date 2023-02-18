916329f6e41ad02c68b7fd1da25a38715b8602a4
XeanN <angel.xp.pb@gmail.com>
2023-02-18 10:53:36 -0500

    seguimos avanzando

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,15 +1,45 @@
 const User = require("../models/userModel.js");
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jwtToken.js");
 const createUser = asyncHandler( async(req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email});
     if(!findUser){
         // Crear nuevo usuario
-        const newUser = await  User.create(req.body);
+        const newUser = await User.create(req.body);
         res.json(newUser);
     } else{
-        throw new Error("User Already Exists")
+        throw new Error("User Already Exists");
     }
 });
 
-module.exports = { createUser };
+//Login a user
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+    // check if user exists or not
+    const findUser = await User.findOne({email});
+    if( findUser && (await findUser.isPasswordMatched(password))){
+        res.json( {
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        })
+    }else{
+        throw new Error("Invalid Credentials");
+    }
+});
+
+// Get all users
+const getallUser = asyncHandler(async(req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { createUser} = require("../controller/userCtrl.js")
+const { createUser, loginUserCtrl} = require("../controller/userCtrl.js")
 const router = express.Router();
 
 router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
 module.exports = router;
